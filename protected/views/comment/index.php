d108f23a3322f8c80e5b7908c0f8febb4ac491c3
garydai <daitechang@gmail.com>
2014-12-09 17:49:39 +0800

    add comment css

--- protected/views/comment/index.php
+++ protected/views/comment/index.php
@@ -1,9 +1,9 @@
-<link type="text/css" rel="stylesheet" href="/comment.css" />
+<link type="text/css" rel="stylesheet" href="/css/comment.css" />
 
-<div class="title">
+<div class="comment_title">
 
-	<a href="<?php echo $title->url?>"  target="_blank"  ><span class="song"><?php echo $title->song?></span></a>
-	<br><span class="singer"><?php echo $title->singer?></span>
+	<a href="<?php echo $title->url?>"  target="_blank"  ><span class="comment_song"><?php echo $title->song?></span></a>
+	<br><span class="comment_singer"><?php echo $title->singer?></span>
 </div>
 
 <div class="add_comment">
@@ -24,7 +24,11 @@
 <div class="comments">
 
 	<?php for($i=0; $i < count($comments); $i ++) {?>
-		<div class="comment"><?php echo $comments[$i]->content?></div>
+		<div class="comment_items">
+		<span class="comment_user"><?php echo $comments[$i]->user ?> </span>|<span class="comment_date"> <?php echo $comments[$i]->date ?></span>
+		<div class="comment_comment"><?php echo $comments[$i]->content?></div>
+		</div>
+
 	<?php } ?>
 
 
